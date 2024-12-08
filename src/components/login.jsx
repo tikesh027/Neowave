215c008d6d891689a3e9cab48d0c4bf1131a6a88
tikesh027 <tikeshsingh027@yahoo.com>
2024-12-08 19:27:31 +0530

    added social media login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import loginImage from "../images/loginImage.jpg";
-import spotifyButton from "../images/spotifyButton.jpg";
+import music from "../images/music.jpg";
+import SpotifyLogo from "../images/spotifyLogo.png";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import LoadingButton from "@mui/lab/LoadingButton";
+import Button from "@mui/material/Button";
+import googleIcon from "../images/googleIcon.png";
+import facebookIcon from "../images/facebookIcon.png";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
@@ -18,24 +21,24 @@ const Login = () => {
   return (
     <div className="relative flex justify-center items-center h-screen w-screen overflow-hidden">
       <img
-        src={loginImage}
+        src={music}
         alt="Login Background"
         className="h-full w-full object-cover"
       />
-      <div className="absolute z-10 p-4 bg-opacity-80 bg-slate-500 w-[400px] h-[600px] flex flex-col items-center rounded-[10px]">
-        <div>
-          <img
-            src={spotifyButton}
-            alt="Spotify Button"
-            className="h-[150px] w-[450px]"
-          />
-        </div>
+      <div className="absolute z-10 p-4 bg-opacity-50 bg-black w-[400px] h-[550px] flex flex-col items-center rounded-[20px]">
         <div className="mt-6 flex flex-col justify-center">
           <form>
+            <div className="flex justify-center mt-2 mb-6">
+              <img
+                src={SpotifyLogo}
+                alt="Spotify Logo"
+                className="w-[300px] h-[85px]"
+              />
+            </div>
             <FormControl sx={{ width: "36ch" }}>
               <OutlinedInput
                 placeholder="Your Email"
-                className="bg-white mt-5 h-12"
+                className="bg-white h-12"
               />
               <OutlinedInput
                 placeholder="Your Password"
@@ -52,6 +55,23 @@ const Login = () => {
                 >
                   {loading ? "Loading..." : "Login"}
                 </LoadingButton>
+              </div>
+              <div>
+                <hr className="text-white w-full h-4 mt-8 mb-8" />
+              </div>
+              <div className="flex justify-center items-center gap-4 flex-col">
+                <div className="bg-white w-full text-center text-black">
+                  <Button className="flex justify-center gap-2">
+                    <img src={googleIcon} className="w-4 h-4" />
+                    <p className="text-black">Google Login</p>
+                  </Button>
+                </div>
+                <div className="bg-blue-400 w-full text-center text-white">
+                  <Button className="flex justify-center gap-2">
+                    <img src={facebookIcon} className="w-4 h-4" />
+                    <p className="text-white">Facebook Login</p>
+                  </Button>
+                </div>
               </div>
             </FormControl>
           </form>
