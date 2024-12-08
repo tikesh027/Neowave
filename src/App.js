62fa1b73085801650e4fc602e9dfb697dfd09cd7
tikesh027 <tikeshsingh027@yahoo.com>
2024-12-09 00:58:45 +0530

    context api setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/login";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/constants";
+import HomePage from "./components/homePage";
 
 const App = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+  return <div>{token ? <HomePage /> : <Login />}</div>;
 };
 
 export default App;
